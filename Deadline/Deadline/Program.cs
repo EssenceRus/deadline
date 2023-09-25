e76f5ee4247b20923cca7e6fd54a40e414f00378
EssenceRus <107023381+EssenceRus@users.noreply.github.com>
2023-09-25 15:20:39 +0500

    Add files via upload

---
+++ Deadline/Deadline/Program.cs
@@ -0,0 +1,50 @@
+﻿using Deadline;
+
+List<Tasks> tasks = new List<Tasks>
+{
+    new Tasks(20,2),
+    new Tasks(10,1),
+    new Tasks(1,3),
+    new Tasks(15,2),
+    new Tasks(5,3),
+    new Tasks(2,3),
+    new Tasks(1,2)
+};
+var tasksSort = tasks.OrderByDescending(p=>p.Price);
+int finalPrice = 0;
+int totalTasks = tasks.OrderByDescending(t=>t.Deadline).First().Deadline;
+int time = totalTasks-1;
+
+//foreach (var item in tasks.OrderByDescending(c => c.Price))
+//{
+//    if (time < item.Deadline)
+//    {
+//        finalPrice += item.Price;
+//        Console.WriteLine($"deadline {item.Deadline}  price {item.Price}");
+//        totalTasks--;
+//    } else
+//    {
+//        time--;
+//    }
+
+//    //if (totalTasks==0)
+//    //{
+//    //    break;
+//    //}
+//}
+for (int i = 0; i < totalTasks; i++)
+{
+    foreach (var item in tasksSort)
+    {
+        if (time < item.Deadline)
+        {
+            finalPrice += item.Price;
+            Console.WriteLine($"deadline {item.Deadline}  price {item.Price}");
+            tasks.Remove(item);
+            time--;
+            break;
+        }
+    }
+}
+
+Console.WriteLine(finalPrice);
---
+++ Deadline/Deadline/Tasks.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deadline
+{
+    public class Tasks
+    {
+        public Tasks (int price, int deadline)
+        {
+            Price = price;
+            Deadline = deadline;
+        }
+
+
+        public int Price { get; set; }
+        public int Deadline { get; set; }
+    }
+}
